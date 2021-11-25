0b60adfeb861d2aa61f3535c8bf1ddb7e6b53ef2
Juan Pablo Montoya Cardona <jpmontoya182@gmail.com>
2021-11-25 18:41:32 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Spark.Sql;
+
+namespace HelloSpark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spark = SparkSession
+                .Builder()
+                .AppName("DemoApp")
+                .GetOrCreate();
+
+            var dataFrame = spark.Sql("select id, rand() as random_number from range(1000)");
+
+            dataFrame
+                .Write()
+                .Format("csv")
+                .Option("header", true)
+                .Option("sep", "|")
+                .Mode("overwrite")
+                .Save(args[1]);
+
+            foreach (var row in dataFrame.Collect())
+            {
+                if (row[0] as int? % 2 == 0)
+                {
+                    Console.WriteLine($"row : {row[0]}");
+                }
+                
+            }
+        }
+    }
+}
